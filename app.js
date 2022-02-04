6b8106692b2a307b69d7eabc4d9719adb0437536
kia1016 <kia1016@gmail.com>
2022-02-03 22:01:36 -0500

    JSObjectArrayEx

--- app.js
+++ app.js
@@ -3,7 +3,26 @@ console.log("Hello World!\n==========\n");
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
 
+const numbers = [2, 22, 12, 17, 18, 39, 129];
+
+function arraySum(numbers){
+    var sum = 0;
+    for(let i = 0; i < numbers.length; i++) {
+        sum += numbers[i]
+    }
+    return sum
+}
+console.log(arraySum(numbers))
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
 
+const book = {};
+book.title = "The Hunger Games";
+book.pages = 374;
+book.readCount = 1;
+book.info = () => {
+    return `${book.title}, ${book.pages} pages, Read ${book.readCount} times.`;
+};
+console.log(book.info());
 
+}
